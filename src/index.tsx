32725050fc849605b4114be5ff9276302ab3bb4f
SAURABH SHARMA <saurabhshcs@gmail.com>
2021-07-24 00:50:48 +0100

    [TSZ-001- TS & React saurabhshcs - First initial configuration learnings for tyypescript & React

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+ReactDom.render(
+    <div>
+        <h1> This is first React and Typesript</h1>
+        <p> This is simpla example of learning of the tpyescript along with react.</p>
+        <a href="#">Click Me</a>
+    </div>,
+    document.querySelector('#root')
+)
