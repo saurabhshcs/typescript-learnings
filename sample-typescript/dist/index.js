1a6266af54acfc646336294cff5f83b958daf25e
Saurabh Sharma <saurabhshcsibm.com@Saurabhs-MacBook-Pro.local>
2021-09-23 00:05:26 +0100

    Initial typescript project setup.

---
+++ sample-typescript/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiply(a, b) {
+    return a * b;
+}
+const a = 5;
+const b = 2;
+console.log(`${a} * ${b} = ${multiply(a, b)}`);
---
+++ sample-typescript/src/index.ts
@@ -0,0 +1,9 @@
+function multiply(a: number, b: number): number {
+    return a * b;
+  }
+  
+  const a = 5;
+  const b = 2;
+  
+  console.log(`${a} * ${b} = ${multiply(a, b)}`);
+  
